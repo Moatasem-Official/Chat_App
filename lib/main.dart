b0f1790f283e2530d0fe14c7ab3972594b3a3ee4
Moatasem <moatasemnagy3@gmail.com>
2025-08-07 04:48:17 +0300

    Add Firebase To App

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:chat_app/presentation/screens/login_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
 }
 
