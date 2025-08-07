31a1d540a8a0fef1de5e207681ca7344d419fb3f
Moatasem <moatasemnagy3@gmail.com>
2025-08-07 22:09:04 +0300

    Add Auth User State Determination Listener Function At Main Function

--- lib/data/services/firebase/auth/auth_service.dart
+++ lib/data/services/firebase/auth/auth_service.dart
@@ -1,7 +1,7 @@
 class AuthService {
-  Future<void> signOut() async {}
-  Future<void> sendEmailVerification() async {}
-  Future<void> sendPasswordResetEmail(String email) async {}
-  Future<void> signUp(String email, String password) async {}
-  Future<void> signIn(String email, String password) async {}
+  static Future<void> signOut() async {}
+  static Future<void> sendEmailVerification() async {}
+  static Future<void> sendPasswordResetEmail(String email) async {}
+  static Future<void> signUp(String email, String password) async {}
+  static Future<void> signIn(String email, String password) async {}
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:chat_app/presentation/screens/login_screen.dart';
 import 'package:device_preview/device_preview.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -10,8 +11,25 @@ void main() async {
   runApp(DevicePreview(enabled: true, builder: (context) => const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print('User is signed in!');
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
